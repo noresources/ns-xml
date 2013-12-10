c676660760a26a60496e6647c882d997e6cf9df9
Renaud Guillard <dev@nore.fr>
2013-12-10 20:16:00 +0100

    just for fun: add a tool to generate gource videos. add ns-xml logo images

---
+++ resources/xsh/create-gource-video.xsh
@@ -0,0 +1,59 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!-- Copyright © 2011-2012 by Renaud Guillard (dev@nore.fr) -->
+<!-- Distributed under the terms of the MIT License, see LICENSE -->
+<xsh:program interpreterType="ksh" xmlns:prg="http://xsd.nore.fr/program" xmlns:xsh="http://xsd.nore.fr/xsh" xmlns:bash="http://xsd.nore.fr/bash" xmlns:xi="http://www.w3.org/2001/XInclude">
+	<xsh:info>
+		<xi:include href="create-gource-video.xml" />
+	</xsh:info>
+	<xsh:functions>
+		<xi:include href="../../ns/xsh/lib/filesystem/filesystem.xsh" xpointer="xmlns(xsh=http://xsd.nore.fr/xsh)xpointer(//xsh:function[@name = 'ns_realpath'])" />
+		<xi:include href="../../ns/xsh/lib/filesystem/filesystem.xsh" xpointer="xmlns(xsh=http://xsd.nore.fr/xsh)xpointer(//xsh:function[@name = 'ns_relativepath'])" />
+		<xsh:function name="filesystempath_to_nmepath">
+			<xsh:parameter name="sourceBasePath" />
+			<xsh:parameter name="outputBasePath" />
+			<xsh:parameter name="path" />
+			<xsh:body><![CDATA[
+local output="$(echo "${path#${sourceBasePath}}" | tr -d "/" | tr " " "_")"
+output="${outputBasePath}/${output}"
+echo "${output}"
+			]]></xsh:body>
+		</xsh:function>
+	</xsh:functions>
+	<xsh:code><![CDATA[
+scriptFilePath="$(ns_realpath "${0}")"
+scriptPath="$(dirname "${scriptFilePath}")"
+rootPath="$(ns_realpath "${scriptPath}/../..")"
+cwd="$(pwd)"
+
+if ! parse "${@}"
+then
+	if ${displayHelp}
+	then
+		usage
+		exit 0
+	fi
+	
+	parse_displayerrors
+	exit 1
+fi
+
+if ${displayHelp}
+then
+	usage
+	exit 0
+fi
+
+for x in gource ffmpeg
+do
+	if ! which ${x} 1>/dev/null 2>&1
+	then
+		echo "${x} not found"
+		exit 1
+	fi
+done
+
+outputFile="${parser_values[0]}"
+[ -z "${outputFile}" ] && outputFile="gource-$(date +%F).mp4"
+gource --load-config "${configurationFile}" --output-framerate 30 --output-ppm-stream - | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset ultrafast -crf 1 -threads 0 -bf 0 "${outputFile}"
+]]></xsh:code>
+</xsh:program>
---
+++ tools/sh/create-gource-video.sh
@@ -0,0 +1,537 @@
+#!/usr/bin/env ksh
+# ####################################
+# Copyright © 2012 by Renaud Guillard (dev@nore.fr)
+# Distributed under the terms of the MIT License, see LICENSE
+# Author: Renaud Guillard
+# Version: 2.0
+# 
+# Documentation builder
+#
+# Program help
+function usage
+{
+cat << EOFUSAGE
+create-gource-video: Documentation builder
+Usage: 
+  create-gource-video [--help] -c <path> [Output file]
+  With:
+    --help: Display program usage
+    -c, --configuration: Gource configuration file
+EOFUSAGE
+}
+
+# Program parameter parsing
+parser_shell="$(readlink /proc/$$/exe | sed "s/.*\/\([a-z]*\)[0-9]*/\1/g")"
+parser_input=("${@}")
+parser_itemcount=${#parser_input[*]}
+parser_startindex=0
+parser_index=0
+parser_subindex=0
+parser_item=""
+parser_option=""
+parser_optiontail=""
+parser_subcommand=""
+parser_subcommand_expected=false
+PARSER_OK=0
+PARSER_ERROR=1
+PARSER_SC_OK=0
+PARSER_SC_ERROR=1
+PARSER_SC_UNKNOWN=2
+PARSER_SC_SKIP=3
+# Compatibility with shell which use "1" as start index
+[ "${parser_shell}" = "zsh" ] && parser_startindex=1
+parser_itemcount=$(expr ${parser_startindex} + ${parser_itemcount})
+parser_index=${parser_startindex}
+
+# Required global options
+# (Subcommand required options will be added later)
+
+parser_required[$(expr ${#parser_required[*]} + ${parser_startindex})]="G_2_configuration:--configuration"
+# Switch options
+displayHelp=false
+# Single argument options
+configurationFile=
+
+function parse_addwarning
+{
+	typeset var message="${1}"
+	typeset var m="[${parser_option}:${parser_index}:${parser_subindex}] ${message}"
+	typeset var c=${#parser_warnings[*]}
+	c=$(expr ${c} + ${parser_startindex})
+	parser_warnings[${c}]="${m}"
+}
+function parse_adderror
+{
+	typeset var message="${1}"
+	typeset var m="[${parser_option}:${parser_index}:${parser_subindex}] ${message}"
+	typeset var c=${#parser_errors[*]}
+	c=$(expr ${c} + ${parser_startindex})
+	parser_errors[${c}]="${m}"
+}
+function parse_addfatalerror
+{
+	typeset var message="${1}"
+	typeset var m="[${parser_option}:${parser_index}:${parser_subindex}] ${message}"
+	typeset var c=${#parser_errors[*]}
+	c=$(expr ${c} + ${parser_startindex})
+	parser_errors[${c}]="${m}"
+	parser_aborted=true
+}
+
+function parse_displayerrors
+{
+	for ((i=${parser_startindex};${i}<${#parser_errors[*]};i++))
+	do
+		echo -e "\t- ${parser_errors[${i}]}"
+	done
+}
+
+
+function parse_pathaccesscheck
+{
+	typeset var file="${1}"
+	[ ! -a "${file}" ] && return 0
+	
+	typeset var accessString="${2}"
+	while [ ! -z "${accessString}" ]
+	do
+		[ -${accessString:0:1} ${file} ] || return 1;
+		accessString=${accessString:1}
+	done
+	return 0
+}
+function parse_setoptionpresence
+{
+	for ((i=${parser_startindex};${i}<$(expr ${parser_startindex} + ${#parser_required[*]});i++))
+	do
+		typeset var idPart="$(echo "${parser_required[${i}]}" | cut -f 1 -d":" )"
+		if [ "${idPart}" = "${1}" ]
+		then
+			parser_required[${i}]=""
+			return 0
+		fi
+	done
+	return 1
+}
+function parse_checkrequired
+{
+	# First round: set default values
+	for ((i=${parser_startindex};${i}<$(expr ${parser_startindex} + ${#parser_required[*]});i++))
+	do
+		typeset var todoPart="$(echo "${parser_required[${i}]}" | cut -f 3 -d":" )"
+		[ -z "${todoPart}" ] || eval "${todoPart}"
+	done
+	typeset var c=0
+	for ((i=${parser_startindex};${i}<$(expr ${parser_startindex} + ${#parser_required[*]});i++))
+	do
+		if [ ! -z "${parser_required[${i}]}" ]
+		then
+			typeset var displayPart="$(echo "${parser_required[${i}]}" | cut -f 2 -d":" )"
+			parser_errors[$(expr ${#parser_errors[*]} + ${parser_startindex})]="Missing required option ${displayPart}"
+			c=$(expr ${c} + 1)
+		fi
+	done
+	return ${c}
+}
+function parse_setdefaultarguments
+{
+	typeset var parser_set_default=false
+}
+function parse_checkminmax
+{
+	typeset var errorCount=0
+	# Check min argument for multiargument
+	
+	return ${errorCount}
+}
+function parse_numberlesserequalcheck
+{
+	typeset var hasBC=false
+	which bc 1>/dev/null 2>&1 && hasBC=true
+	if ${hasBC}
+	then
+		[ "$(echo "${1} <= ${2}" | bc)" = "0" ] && return 1
+	else
+		typeset var a_int="$(echo "${1}" | cut -f 1 -d".")"
+		typeset var a_dec="$(echo "${1}" | cut -f 2 -d".")"
+		[ "${a_dec}" = "${1}" ] && a_dec="0"
+		typeset var b_int="$(echo "${2}" | cut -f 1 -d".")"
+		typeset var b_dec="$(echo "${2}" | cut -f 2 -d".")"
+		[ "${b_dec}" = "${2}" ] && b_dec="0"
+		[ ${a_int} -lt ${b_int} ] && return 0
+		[ ${a_int} -gt ${b_int} ] && return 1
+		([ ${a_int} -ge 0 ] && [ ${a_dec} -gt ${b_dec} ]) && return 1
+		([ ${a_int} -lt 0 ] && [ ${b_dec} -gt ${a_dec} ]) && return 1
+	fi
+	return 0
+}
+function parse_enumcheck
+{
+	typeset var ref="${1}"
+	shift 1
+	while [ $# -gt 0 ]
+	do
+		[ "${ref}" = "${1}" ] && return 0
+		shift
+	done
+	return 1
+}
+function parse_addvalue
+{
+	typeset var position=${#parser_values[*]}
+	typeset var value
+	if [ $# -gt 0 ] && [ ! -z "${1}" ]; then value="${1}"; else return ${PARSER_ERROR}; fi
+	shift
+	if [ -z "${parser_subcommand}" ]
+	then
+		case "${position}" in
+		0)
+			;;
+		*)
+			;;
+		
+		esac
+	else
+		case "${parser_subcommand}" in
+		*)
+			return ${PARSER_ERROR}
+			;;
+		
+		esac
+	fi
+	parser_values[$(expr ${#parser_values[*]} + ${parser_startindex})]="${value}"
+}
+function parse_process_subcommand_option
+{
+	parser_item="${parser_input[${parser_index}]}"
+	if [ -z "${parser_item}" ] || [ "${parser_item:0:1}" != "-" ] || [ "${parser_item}" = "--" ]
+	then
+		return ${PARSER_SC_SKIP}
+	fi
+	
+	return ${PARSER_SC_SKIP}
+}
+function parse_process_option
+{
+	if [ ! -z "${parser_subcommand}" ] && [ "${parser_item}" != "--" ]
+	then
+		parse_process_subcommand_option && return ${PARSER_OK}
+		[ ${parser_index} -ge ${parser_itemcount} ] && return ${PARSER_OK}
+	fi
+	
+	parser_item="${parser_input[${parser_index}]}"
+	
+	[ -z "${parser_item}" ] && return ${PARSER_OK}
+	
+	if [ "${parser_item}" = "--" ]
+	then
+		for ((a=$(expr ${parser_index} + 1);${a}<${parser_itemcount};a++))
+		do
+			parse_addvalue "${parser_input[${a}]}"
+		done
+		parser_index=${parser_itemcount}
+		return ${PARSER_OK}
+	elif [ "${parser_item}" = "-" ]
+	then
+		return ${PARSER_OK}
+	elif [ "${parser_item:0:2}" = "\-" ]
+	then
+		parse_addvalue "${parser_item:1}"
+	elif [ "${parser_item:0:2}" = "--" ] 
+	then
+		parser_option="${parser_item:2}"
+		if echo "${parser_option}" | grep "=" 1>/dev/null 2>&1
+		then
+			parser_optiontail="$(echo "${parser_option}" | cut -f 2- -d"=")"
+			parser_option="$(echo "${parser_option}" | cut -f 1 -d"=")"
+		fi
+		
+		case "${parser_option}" in
+		help)
+			if [ ! -z "${parser_optiontail}" ]
+			then
+				parse_adderror "Unexpected argument (ignored) for option \"${parser_option}\""
+				parser_optiontail=""
+				return ${PARSER_ERROR}
+			fi
+			displayHelp=true
+			parse_setoptionpresence G_1_help
+			;;
+		configuration)
+			if [ ! -z "${parser_optiontail}" ]
+			then
+				parser_item="${parser_optiontail}"
+			else
+				parser_index=$(expr ${parser_index} + 1)
+				if [ ${parser_index} -ge ${parser_itemcount} ]
+				then
+					parse_adderror "End of input reached - Argument expected"
+					return ${PARSER_ERROR}
+				fi
+				
+				parser_item="${parser_input[${parser_index}]}"
+				if [ "${parser_item}" = "--" ]
+				then
+					parse_adderror "End of option marker found - Argument expected"
+					parser_index=$(expr ${parser_index} - 1)
+					return ${PARSER_ERROR}
+				fi
+			fi
+			
+			parser_subindex=0
+			parser_optiontail=""
+			[ "${parser_item:0:2}" = "\-" ] && parser_item="${parser_item:1}"
+			if [ ! -e "${parser_item}" ]
+			then
+				parse_adderror "Invalid path \"${parser_item}\" for option \"${parser_option}\""
+				return ${PARSER_ERROR}
+			fi
+			
+			if ! parse_pathaccesscheck "${parser_item}" "r"
+			then
+				parse_adderror "Invalid path permissions for \"${parser_item}\", r privilege(s) expected for option \"${parser_option}\""
+				return ${PARSER_ERROR}
+			fi
+			
+			if [ -a "${parser_item}" ] && ! ([ -f "${parser_item}" ])
+			then
+				parse_adderror "Invalid patn type for option \"${parser_option}\""
+				return ${PARSER_ERROR}
+			fi
+			
+			configurationFile="${parser_item}"
+			parse_setoptionpresence G_2_configuration
+			;;
+		*)
+			parse_addfatalerror "Unknown option \"${parser_option}\""
+			return ${PARSER_ERROR}
+			;;
+		
+		esac
+	elif [ "${parser_item:0:1}" = "-" ] && [ ${#parser_item} -gt 1 ]
+	then
+		parser_optiontail="${parser_item:$(expr ${parser_subindex} + 2)}"
+		parser_option="${parser_item:$(expr ${parser_subindex} + 1):1}"
+		if [ -z "${parser_option}" ]
+		then
+			parser_subindex=0
+			return ${PARSER_SC_OK}
+		fi
+		
+		case "${parser_option}" in
+		c)
+			if [ ! -z "${parser_optiontail}" ]
+			then
+				parser_item="${parser_optiontail}"
+			else
+				parser_index=$(expr ${parser_index} + 1)
+				if [ ${parser_index} -ge ${parser_itemcount} ]
+				then
+					parse_adderror "End of input reached - Argument expected"
+					return ${PARSER_ERROR}
+				fi
+				
+				parser_item="${parser_input[${parser_index}]}"
+				if [ "${parser_item}" = "--" ]
+				then
+					parse_adderror "End of option marker found - Argument expected"
+					parser_index=$(expr ${parser_index} - 1)
+					return ${PARSER_ERROR}
+				fi
+			fi
+			
+			parser_subindex=0
+			parser_optiontail=""
+			[ "${parser_item:0:2}" = "\-" ] && parser_item="${parser_item:1}"
+			if [ ! -e "${parser_item}" ]
+			then
+				parse_adderror "Invalid path \"${parser_item}\" for option \"${parser_option}\""
+				return ${PARSER_ERROR}
+			fi
+			
+			if ! parse_pathaccesscheck "${parser_item}" "r"
+			then
+				parse_adderror "Invalid path permissions for \"${parser_item}\", r privilege(s) expected for option \"${parser_option}\""
+				return ${PARSER_ERROR}
+			fi
+			
+			if [ -a "${parser_item}" ] && ! ([ -f "${parser_item}" ])
+			then
+				parse_adderror "Invalid patn type for option \"${parser_option}\""
+				return ${PARSER_ERROR}
+			fi
+			
+			configurationFile="${parser_item}"
+			parse_setoptionpresence G_2_configuration
+			;;
+		*)
+			parse_addfatalerror "Unknown option \"${parser_option}\""
+			return ${PARSER_ERROR}
+			;;
+		
+		esac
+	elif ${parser_subcommand_expected} && [ -z "${parser_subcommand}" ] && [ ${#parser_values[*]} -eq 0 ]
+	then
+		case "${parser_item}" in
+		*)
+			parse_addvalue "${parser_item}"
+			;;
+		
+		esac
+	else
+		parse_addvalue "${parser_item}"
+	fi
+	return ${PARSER_OK}
+}
+function parse
+{
+	parser_aborted=false
+	while [ ${parser_index} -lt ${parser_itemcount} ] && ! ${parser_aborted}
+	do
+		parse_process_option
+		if [ -z "${parser_optiontail}" ]
+		then
+			parser_index=$(expr ${parser_index} + 1)
+			parser_subindex=0
+		else
+			parser_subindex=$(expr ${parser_subindex} + 1)
+		fi
+	done
+	
+	if ! ${parser_aborted}
+	then
+		parse_setdefaultarguments
+		parse_checkrequired
+		parse_checkminmax
+	fi
+	
+	
+	
+	typeset var parser_errorcount=${#parser_errors[*]}
+	return ${parser_errorcount}
+}
+
+function ns_realpath
+{
+	typeset var inputPath
+	if [ $# -gt 0 ]
+	then
+		inputPath="${1}"
+		shift
+	fi
+	typeset var cwd="$(pwd)"
+	[ -d "${inputPath}" ] && cd "${inputPath}" && inputPath="."
+	while [ -h "${inputPath}" ] ; do inputPath="$(readlink "${inputPath}")"; done
+	
+	if [ -d "${inputPath}" ]
+	then
+		inputPath="$(cd -P "$(dirname "${inputPath}")" && pwd)"
+	else
+		inputPath="$(cd -P "$(dirname "${inputPath}")" && pwd)/$(basename "${inputPath}")"
+	fi
+	
+	cd "${cwd}" 1>/dev/null 2>&1
+	echo "${inputPath}"
+}
+function ns_relativepath
+{
+	typeset var from
+	if [ $# -gt 0 ]
+	then
+		from="${1}"
+		shift
+	fi
+	typeset var base
+	if [ $# -gt 0 ]
+	then
+		base="${1}"
+		shift
+	else
+		base="."
+	fi
+	[ -r "${from}" ] || return 1
+	[ -r "${base}" ] || return 2
+	[ ! -d "${base}" ] && base="$(dirname "${base}")"  
+	[ -d "${base}" ] || return 3
+	from="$(ns_realpath "${from}")"
+	base="$(ns_realpath "${base}")"
+	#echo from: $from
+	#echo base: $base
+	c=0
+	sub="${base}"
+	newsub=""
+	while [ "${from:0:${#sub}}" != "${sub}" ]
+	do
+		newsub="$(dirname "${sub}")"
+		[ "${newsub}" == "${sub}" ] && return 4
+		sub="${newsub}"
+		c="$(expr ${c} + 1)"
+	done
+	res="."
+	for ((i=0;${i}<${c};i++))
+	do
+		res="${res}/.."
+	done
+	res="${res}${from#${sub}}"
+	res="${res#./}"
+	echo "${res}"
+}
+function filesystempath_to_nmepath
+{
+	typeset var sourceBasePath
+	if [ $# -gt 0 ]
+	then
+		sourceBasePath="${1}"
+		shift
+	fi
+	typeset var outputBasePath
+	if [ $# -gt 0 ]
+	then
+		outputBasePath="${1}"
+		shift
+	fi
+	typeset var path
+	if [ $# -gt 0 ]
+	then
+		path="${1}"
+		shift
+	fi
+	local output="$(echo "${path#${sourceBasePath}}" | tr -d "/" | tr " " "_")"
+	output="${outputBasePath}/${output}"
+	echo "${output}"
+}
+scriptFilePath="$(ns_realpath "${0}")"
+scriptPath="$(dirname "${scriptFilePath}")"
+rootPath="$(ns_realpath "${scriptPath}/../..")"
+cwd="$(pwd)"
+
+if ! parse "${@}"
+then
+	if ${displayHelp}
+	then
+		usage
+		exit 0
+	fi
+	
+	parse_displayerrors
+	exit 1
+fi
+
+if ${displayHelp}
+then
+	usage
+	exit 0
+fi
+
+for x in gource ffmpeg
+do
+	if ! which ${x} 1>/dev/null 2>&1
+	then
+		echo "${x} not found"
+		exit 1
+	fi
+done
+
+outputFile="${parser_values[0]}"
+[ -z "${outputFile}" ] && outputFile="gource-$(date +%F).mp4"
+gource --load-config "${configurationFile}" --output-framerate 30 --output-ppm-stream - | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - -vcodec libx264 -preset ultrafast -crf 1 -threads 0 -bf 0 "${outputFile}"
