905031db84155152dcd80d2707c74b1257801302
Renaud Guillard <dev@nore.fr>
2017-03-31 13:42:49 +0200

    remove legacy code

--- resources/legacy/python/program/2.0/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-""" Copyright © 2012 by Renaud Guillard """
-""" Distributed under the terms of the MIT License, see LICENSE """
-
-from Program import Program
-from Base import Usage
