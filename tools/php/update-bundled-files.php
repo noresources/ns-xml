11e77ebd2be9ef505d411ca3e0bf023356dd3cf7
Renaud Guillard <dev@nore.fr>
2019-06-29 22:09:24 +0200

    ...

--- tools/php/update-bundled-files.php
+++ tools/php/update-bundled-files.php
@@ -1,11 +1,10 @@
 <?php
-
 namespace NoreSources\NSXML\Composer;
 
 require (__DIR__ . '/../../vendor/autoload.php');
 require (__DIR__ . '/composer/ComposerHook.php');
 
 $result = ComposerHook::updateAmalgamation();
-
+exit (0);
 
 
